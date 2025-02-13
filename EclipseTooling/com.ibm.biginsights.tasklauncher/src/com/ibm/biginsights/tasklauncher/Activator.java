22e047c50deb62c007fe1a9b09478fe69093a37e
Zachary Branson <zac.branson.ibm@gmail.com>
2025-02-13 12:20:16 -0500

    replace more copyright strings
    Signed-off-by: Zachary Branson <zac.branson.ibm@gmail.com>

--- EclipseTooling/com.ibm.biginsights.tasklauncher/src/com/ibm/biginsights/tasklauncher/Activator.java
+++ EclipseTooling/com.ibm.biginsights.tasklauncher/src/com/ibm/biginsights/tasklauncher/Activator.java
@@ -22,16 +22,16 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-	/* begin_generated_IBM_copyright_code                               */
-	public static final String IBM_COPYRIGHT =
-		" Licensed Materials-Property of IBM                              " + //$NON-NLS-1$ 
-		" 5724-Y95                                                        " + //$NON-NLS-1$ 
-		" (C) Copyright IBM Corp.  2012, 2012    All Rights Reserved.     " + //$NON-NLS-1$ 
-		" US Government Users Restricted Rights - Use, duplication or     " + //$NON-NLS-1$ 
-		" disclosure restricted by GSA ADP Schedule Contract with         " + //$NON-NLS-1$ 
-		" IBM Corp.                                                       " + //$NON-NLS-1$ 
-		"                                                                 " ; //$NON-NLS-1$ 
-	/* end_generated_IBM_copyright_code                                 */
+public static final String IBM_COPYRIGHT = "Copyright IBM\n"+
+ "Licensed under the Apache License, Version 2.0 (the \"License\");\n"+
+ "you may not use this file except in compliance with the License.\n"+
+ "You may obtain a copy of the License at\n\n"+
+ "    http://www.apache.org/licenses/LICENSE-2.0\n\n"+
+ "Unless required by applicable law or agreed to in writing, software\n"+
+ "distributed under the License is distributed on an \"AS IS\" BASIS,\n"+
+ "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"+
+ "See the License for the specific language governing permissions and\n"+
+ "limitations under the License.";
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.ibm.bigInsights.quick.launch.ui"; //$NON-NLS-1$
